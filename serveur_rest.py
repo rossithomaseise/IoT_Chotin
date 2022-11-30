0a6008e8fff292b31cab84920ce98932c79ba0c2
rossithomaseise <rossi.thomas83140@gmail.com>
2022-11-30 15:06:49 +0100

    sex

--- serveur_rest.py
+++ serveur_rest.py
@@ -1,9 +1,10 @@
-# curl -X GET http://localhost:8888/Etudiant
+# curl -X GET http://localhost:8888/Facture
 # curl -X POST http://localhost:8888/Etudiant/\?Nom\=Cionaire\&Prenom\=Dick\&idAd\=2	
+# curl -X POST http://localhost:8888/Facture/\?Type\=Assurance\&Date_Facture\=2023-01-01\&Montant\=20\&Consommation\=0.00	
+# curl -X GET http://localhost:8888/Facture/Type
+# curl -X GET http://localhost:8888/Logement/Numero_Telephone/IP
 
 import http.server, urllib.parse, sqlite3
-
-import numpy as np
 from meteo import *
 
 class MyHandler(http.server.BaseHTTPRequestHandler):
@@ -13,34 +14,46 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
 
 	def do_POST(self):
 		"""Respond to a POST request."""
-		"""res = urllib.parse.urlparse(self.path)
+		res = urllib.parse.urlparse(self.path)
 		query = urllib.parse.parse_qs(res.query)
 		print(query,"query")
 		rep = self.mysql.insert(res.path,query)
-		print("rep",rep)"""
-		print("post")
+		print("rep",rep)
 		self.send_response(200)
 		self.send_header("Content-type", "text/html")
 		self.end_headers()		
-		self.wfile.write(bytes(str("allo post")+'\n', 'UTF-8'))
-		
+		self.wfile.write(bytes(str(rep)+'\n', 'UTF-8'))
+
 	def do_GET(self):
 		"""Respond to a GET request."""
 		res = urllib.parse.urlparse(self.path)
-		print("res",res)
-		print("res path",res.path)
 		if(res.path == "/Meteo"):
 			meteo = meteoH()
 			self.send_response(200)
 			self.send_header("Content-type", "text/html")
 			self.end_headers()
 			self.wfile.write(bytes(str(meteo)+'\n', 'UTF-8'))
-		elif(res.path == "/Facture"):
+		elif(res.path == "/graph_facture"):
 			facture = self.mysql.facture()
-			string_facture = "["
+
+			facture2 = [['Internet',0.00],['Electricite',0.00],['Gaz',0.00],['Eau',0.00],['Assurance',0.00]]
 
 			for i in range(len(facture)):
-				string_facture += "['" + str(facture[i][0]) + "', " + str(facture[i][1]) + "],"
+				if(facture[i][0] == 'Internet'):
+					facture2[0][1] += facture[i][1]
+				elif(facture[i][0] == 'Electricite'):
+					facture2[1][1] += facture[i][1]
+				elif(facture[i][0] == 'Gaz'):
+					facture2[2][1] += facture[i][1]
+				elif(facture[i][0] == 'Eau'):
+					facture2[3][1] += facture[i][1]
+				elif(facture[i][0] == 'Assurance'):
+					facture2[4][1] += facture[i][1]
+
+			string_facture = "["
+
+			for i in range(len(facture2)):
+				string_facture += "['" + str(facture2[i][0]) + "', " + str(facture2[i][1]) + "],"
 			string_facture = string_facture[:len(string_facture)-1]
 			string_facture += "]"
 			html = """<html> <head> <!--Load the AJAX API--> <script type='text/javascript' src='https://www.gstatic.com/charts/loader.js'></script> <script type='text/javascript'>
@@ -100,21 +113,26 @@ class MySQL():
 	def select(self,path):
 		elem = path.split('/')
 		print("elem",elem)
+		selection = ""
 		if len(elem) == 2:
 			req = "select * from %s" %(elem[1])
 		else:
-			req = "select %s from %s where id=%s" %(elem[3],elem[1],elem[2])
+			for i in range(2,len(elem)):
+				selection += elem[i] + ","
+			selection = selection[:len(selection)-1]
+			print(selection)
+			req = "select " + selection + " from %s" %(elem[1])
 		return self.c.execute(req).fetchall()
 	
 	def insert(self,path,query):
 		print(query)
-		"""attr = ', '.join(query.keys())
+		attr = ', '.join(query.keys())
 		val = ', '.join('"%s"' %v[0] for v in query.values())
 		print(attr,val)
 		req = "insert into %s (%s) values (%s)" %(path.split('/')[1], attr, val)
-		print(req)"""
-		"""self.c.execute(req)
-		self.conn.commit()"""
+		print(req)
+		self.c.execute(req)
+		self.conn.commit()
 
 if __name__ == '__main__':
 	print("lancement du serveur")
