980cb2b5f685d12c047455c386c28a0bed00d1c4
rossithomaseise <rossi.thomas83140@gmail.com>
2022-12-27 14:20:25 +0100

    version graph 2

--- website_php/test.php
+++ website_php/test.php
@@ -5,6 +5,28 @@ $db = new SQLite3('../logement.db');
 $stm = $db->prepare('SELECT * FROM Facture');
 $res = $stm->execute();
 
-while ($row = $res->fetchArray()) {
-    echo "$row[0] $row[1] $row[2] $row[3] $row[4]<br>";
+$consommation_internet = array();
+$consommation_electricite = array();
+$consommation_gaz = array();
+$consommation_eau = array();
+
+$index = 0;
+
+while ($row = $res->fetchArray()){
+	if($index == 0){
+		$index = $index + 1;
+		$consommation_internet[] = $row[4];
+	}
+    elseif($index == 1){
+    	$index = $index + 1;
+		$consommation_electricite[] = $row[4];
+	}
+	elseif($index == 2){
+		$index = $index + 1;
+		$consommation_gaz[] = $row[4];
+	}
+	elseif($index == 3){
+		$index = 0;
+		$consommation_eau[] = $row[4];
+	}
 }
